7b7d9eb56975bc0033a96fe1c2a92a7ce47008e2
burdms <burdms@gmail.com>
2021-05-15 14:49:04 +0200

    create: get present

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@
 'use strict';
 
 import head from './modules/head';
+import getPresent from './modules/getPresent';
 
 head();
+getPresent();
---
+++ src/modules/getPresent.js
@@ -0,0 +1,18 @@
+import popupOpen from "./popupOpen";
+
+export default function getPresent() {
+  const openPresent = event => {
+    const target = event.target;
+    console.log(target);
+
+    if (target.closest('.fixed-gift')) {
+      document.removeEventListener('click', openPresent);
+
+      document.querySelector('.fixed-gift').style.display = 'none';
+
+      popupOpen('gift');
+    }
+  };
+
+  document.addEventListener('click', openPresent);
+}
